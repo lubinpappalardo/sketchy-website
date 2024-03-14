525dafc99cb542ff858e1cd481b2cd42ccb11844
Lubin Pappalardo <lubinpappalardo@gmail.com>
2024-03-14 19:33:22 -0400

    lower legs added

--- playground.js
+++ playground.js
@@ -1,4 +1,7 @@
-
+document.getElementById("guy-all-control").addEventListener('input', (e) => {
+    document.getElementById("guy").style.transform = `rotate(${e.target.value}deg)`;
+    document.getElementById("all-value").textContent = e.target.value + 'deg';
+});
 
 document.getElementById("guy-head-control").addEventListener('input', (e) => {
     document.getElementById("guy-head").style.transform = `rotate(${e.target.value}deg)`;
@@ -40,29 +43,80 @@ document.getElementById("guy-upper-body-control").addEventListener('input', (e) 
     document.getElementById("upper-body-value").textContent = e.target.value + 'deg';
 });
 
+document.getElementById("guy-left-leg-control").addEventListener('input', (e) => {
+    document.getElementById("guy-left-leg").style.transform = `rotate(${e.target.value}deg)`;
+    document.getElementById("left-leg-value").textContent = e.target.value + 'deg';
+});
+
+document.getElementById("guy-right-leg-control").addEventListener('input', (e) => {
+    document.getElementById("guy-right-leg").style.transform = `rotate(${e.target.value}deg)`;
+    document.getElementById("right-leg-value").textContent = e.target.value + 'deg';
+});
+
+document.getElementById("guy-left-knee-control").addEventListener('input', (e) => {
+    document.getElementById("guy-left-lower-leg").style.transform = `rotate(${e.target.value}deg)`;
+    document.getElementById("left-knee-value").textContent = e.target.value + 'deg';
+});
+
+document.getElementById("guy-right-knee-control").addEventListener('input', (e) => {
+    document.getElementById("guy-right-lower-leg").style.transform = `rotate(${e.target.value}deg)`;
+    document.getElementById("right-knee-value").textContent = e.target.value + 'deg';
+});
+
 document.getElementById("reset-controls").addEventListener('click', () => {
+    document.getElementById("guy-all-control").value = 0;
+    document.getElementById("guy").style.transform = ``;
+    document.getElementById("all-value").textContent = 0 + 'deg';
+
     document.getElementById("guy-head-control").value = 0;
     document.getElementById("guy-head").style.transform = ``;
     document.getElementById("neck-value").textContent = 0 + 'deg';
+
     document.getElementById("guy-left-shoulder-control").value = 0;
     document.getElementById("guy-left-arm").style.transform = ``;
     document.getElementById("left-shoulder-value").textContent = 0 + 'deg';
+
     document.getElementById("guy-left-elbow-control").value = 0;
     document.getElementById("guy-left-lower-arm-group").style.transform = ``;
     document.getElementById("left-elbow-value").textContent = 0 + 'deg';
+
     document.getElementById("guy-left-wrist-control").value = 0;
     document.getElementById("guy-left-hand").style.transform = ``;
     document.getElementById("left-wrist-value").textContent = 0 + 'deg';
+
     document.getElementById("guy-right-shoulder-control").value = 0;
     document.getElementById("guy-right-arm").style.transform = ``;
     document.getElementById("right-shoulder-value").textContent = 0 + 'deg';
+
     document.getElementById("guy-right-elbow-control").value = 0;
     document.getElementById("guy-right-lower-arm-group").style.transform = ``;
     document.getElementById("right-elbow-value").textContent = 0 + 'deg';
+
     document.getElementById("guy-right-wrist-control").value = 0;
     document.getElementById("guy-right-hand").style.transform = ``;
     document.getElementById("right-wrist-value").textContent = 0 + 'deg';
+
     document.getElementById("guy-upper-body-control").value = 0;
     document.getElementById("guy-upper-body-group").style.transform = ``;
-    document.getElementById("upper-body").textContent = 0 + 'deg';
+    document.getElementById("upper-body-value").textContent = 0 + 'deg';
+
+    document.getElementById("guy-left-leg-control").value = 0;
+    document.getElementById("guy-left-leg").style.transform = ``;
+    document.getElementById("left-leg-value").textContent = 0 + 'deg';
+
+    document.getElementById("guy-right-leg-control").value = 0;
+    document.getElementById("guy-right-leg").style.transform = ``;
+    document.getElementById("right-leg-value").textContent = 0 + 'deg';
+    
+    document.getElementById("guy-left-knee-control").value = 0;
+    document.getElementById("guy-left-lower-leg").style.transform = ``;
+    document.getElementById("left-knee-value").textContent = 0 + 'deg';
+
+    document.getElementById("guy-right-knee-control").value = 0;
+    document.getElementById("guy-right-lower-leg").style.transform = ``;
+    document.getElementById("right-knee-value").textContent = 0 + 'deg';
+});
+
+document.getElementById("guy-show-bounding-boxes").addEventListener('click', () => {
+    guy.classList.toggle('show-bounding-boxes');
 });
